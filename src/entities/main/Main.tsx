30643329af6646260f35a3d0f1887fb7e7f5afe7
DroshaMorgan <daniil.dron2000@gmail.com>
2024-08-15 17:27:27 +0300

    MELO-2

--- src/entities/main/Main.tsx
+++ src/entities/main/Main.tsx
@@ -1,13 +1,29 @@
+import { Button, Image } from "antd";
+import { Link } from "react-router-dom";
+import mainBcg from "../../entities/main/main-bcg.jpg";
+// import mainBcg from "@/entities/main/main-bcg.jpg";
 
 
 const Main = () => {
     return (
-        <div>
-            {/*<Navbar/>*/}
-            <div className="bg-red-500">
-                hello
+
+
+        <div className="flex items-center flex-col justify-center h-svh">
+            <Image src={mainBcg} />
+            <div className="main-bcg__info-block-top red">
+                Откройте для себя новых независимых исполнителей
             </div>
 
+            <Button>
+                <Link to="/" className="main-bcg__info-block-middle">
+                    Начать
+                </Link>
+            </Button>
+
+
+            <div className="main-bcg__info-block-bottom">
+                Используется Jamendo music API
+            </div>
         </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/es/locale/ru_RU';
 import App from './App.tsx'
+import './global.css'
+import type { ThemeConfig } from 'antd';
+import { StyleProvider } from '@ant-design/cssinjs';
+
+export const theme = {
+  token: {
+    borderRadius: 4,
+    colorPrimary: '#205dc1',
+    colorText: '#323232',
+    // colorTextSecondary: '#fcfcfc',
+    // fontFamily: '"IBM Plex Sans", sans-serif',
+  },
+} satisfies ThemeConfig;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={ruRU} theme={theme}>
+      <StyleProvider hashPriority="high">
+        <App />
+      </StyleProvider>
+    </ConfigProvider>
   </StrictMode>,
 )
