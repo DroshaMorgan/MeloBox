48712be4cb56bd42360e2491c66f368933860c52
DroshaMorgan <daniil.dron2000@gmail.com>
2024-08-16 10:55:27 +0300

    MELO-3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import Home from "./entities/home/Home";
 function App() {
   return (
     <BrowserRouter>
-      <header className="sticky top-0 flex h-svh w-16 shrink-0 flex-col border-r bg-secondary">
+
+      <header className="fixed top-0 flex h-full w-16 shrink-0 flex-col border-r bg-secondary">
+
         <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-black" to="/">
           IHD
         </Link>
@@ -16,6 +18,16 @@ function App() {
 
 
       </header>
+      {/* <header className="sticky top-0 flex h-svh w-16 shrink-0 flex-col border-r bg-secondary">
+        <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-black" to="/">
+        IHD
+        </Link>
+        <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-black" to="/home">
+        HOME
+        </Link>
+        
+        
+      </header> */}
       <Routes>
         <Route
           path="/"
@@ -25,8 +37,8 @@ function App() {
           element={<Home />} />
         {/* {appRoutes.map(route =>
           <Route key={route.path}
-            path={route.path}
-            element={route.element} />
+          path={route.path}
+          element={route.element} />
         )} */}
       </Routes>
     </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // fontFamily: {
+    //     sans: ['"IBM Plex Sans"', 'sans-serif'],
+    //     serif: ['"IBM Plex Sans"', 'sans-serif'],
+    //     mono: ['"IBM Plex Mono"', 'monospace'],
+    // },
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        typography: 'rgb(var(--color-typography) / <alpha-value>)',
+      },
+      maxWidth: {
+        text: '70ch',
+      },
+    },
   },
   plugins: [],
 }
