88764cb50cd242b067a944539317966418b51637
DroshaMorgan <daniil.dron2000@gmail.com>
2024-08-16 11:26:29 +0300

    MELO-3

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,32 @@ import Home from "./entities/home/Home";
 function App() {
   return (
     <BrowserRouter>
-      <header className="fixed top-0 flex h-full w-16 shrink-0 flex-col border-r bg-secondary">
+      <div className="flex">
+        <header className="sticky top-0 flex h-full w-16 shrink-0 flex-col border-r bg-secondary">
 
-        <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-slate-100" to="/">
-          MLE
-        </Link>
-        <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-slate-100" to="/home">
-          HM
-        </Link>
+          <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-slate-100" to="/">
+            MLE
+          </Link>
+          <Link className="m-4 size-8 rounded bg-primary text-center text-sm leading-8 text-slate-100" to="/home">
+            HM
+          </Link>
 
-      </header>
-      <Routes>
-        <Route
-          path="/"
-          element={<Main />} />
-        <Route
-          path="/home"
-          element={<Home />} />
-        {/* {appRoutes.map(route =>
-          <Route key={route.path}
-          path={route.path}
-          element={route.element} />
-        )} */}
-      </Routes>
+        </header>
+
+        <Routes>
+          {/* <Route
+            path="/"
+            element={<Main />} />
+          <Route
+            path="/home"
+            element={<Home />} /> */}
+          {appRoutes.map(route =>
+            <Route key={route.path}
+              path={route.path}
+              element={route.element} />
+          )}
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
