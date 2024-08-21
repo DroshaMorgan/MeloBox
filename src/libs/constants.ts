ef50d8514339c30e96872ea7d02facd32f9c15c1
DroshaMorgan <daniil.dron2000@gmail.com>
2024-08-21 19:19:57 +0300

    MELO-5-love-it-add-albums

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -4,6 +4,10 @@ export const API_URL_ARTISTS_FULL =
 export const API_URL_ARTISTS =
     'https://api.jamendo.com/v3.0/artists';
 
+export const API_URL_ALBUMS_FULL = 'https://api.jamendo.com/v3.0/albums/?artist_name=';
+
+export const API_URL_ALBUMS = 'https://api.jamendo.com/v3.0/albums';
+
 export const CLIENT_ID_FULL =
     'client_id=e1ba0143';
 
