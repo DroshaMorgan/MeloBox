a68981abd5d7bcee9314cf5599edc933dd0fc4bf
Daniil Dron <dron@element-soft.com>
2025-04-25 18:40:38 +0300

    MELO-7

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { appRoutes } from "@/router/routes";
-import { NavSide } from "@/components/nav/NavSide";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { appRoutes } from '@/router/routes';
+import { NavSide } from '@/components/nav/NavSide';
 
 function App() {
   return (
@@ -9,15 +9,13 @@ function App() {
 
       <div className="pl-16">
         <Routes>
-          {appRoutes.map(route =>
-            <Route key={route.path}
-              path={route.path}
-              element={route.element} />
-          )}
+          {appRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 import { memo } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface ToolbarProps {
   back?: boolean | string;
@@ -10,15 +10,27 @@ interface ToolbarProps {
 }
 
 const Toolbar = memo<ToolbarProps>(({ back, extra, title }) => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="flex flex-wrap items-center justify-between gap-x-6 border-b px-6">
         <div className="flex items-center gap-x-4 py-4 text-lg">
-          {back && (
+          {/* {back && (
             <Link className="flex items-stretch gap-x-4 opacity-80 hover:underline" to="..">
               <span>{typeof back === 'string' ? back : 'Назад'}</span>
               <hr className="h-auto border-r" />
             </Link>
+          )} */}
+          {back && (
+            <button
+              type="button"
+              onClick={() => navigate(-1)}
+              className="flex items-stretch gap-x-4 opacity-80 hover:underline"
+            >
+              <span>{typeof back === 'string' ? back : 'Назад'}</span>
+              <hr className="h-auto border-r" />
+            </button>
           )}
 
           <h1 className="max-w-prose text-pretty">{title}</h1>
